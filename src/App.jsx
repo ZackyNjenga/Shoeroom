90a4319efe9a746a0179beaebc1d37d71283409d
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-12-14 19:02:15 +0300

    This is something

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-    return (
-        <h1 className="text-7xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-}
+const App = () => (
+    <main className="relative">
+        Nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+            Hero
+        </section>
+        <section className="padding">
+            Popular Products
+        </section>
+        <section className="padding">
+            SuperQuality
+        </section>
+        <section className="padding">
+            Services
+        </section>
+        <section className="padding">
+            SpecialOffer
+        </section>
+        <section className="padding">
+            Customer Reviews
+        </section>
+        <section className="padding-x sm:p-32 py-16 w-full">
+            Subscribe
+        </section>
+        <section className="padding bg-black padding-x padding-t pb-8">
+            Footer
+        </section>
+    </main>
+);
+
+export default App;
